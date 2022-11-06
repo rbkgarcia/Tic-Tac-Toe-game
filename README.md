e6112cea542658ec1bc4bd48cda3896ee6cd9d36
rbkgarcia <gar19066@byui.edu>
2022-11-06 00:37:34 -0400

    commit 2

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# Tic-Tac-Toe-game
-Tic-Tac-Toe-game Python
+# Tic-Tac-Toe Game
+This is a tic-tac-toe game developed in Python.
+  
+This game is played on a grid that is three squares by three squares, where player one uses x's. Player two uses o's.
 
-Byu project
+The purpose of creating this program was to practice the uses of clases, and loops in phyton.
+
+# Development Environment
+
+This program was built using Python 3, with Visual code studio as the development environment.  
+I used the git tools of Visual Studio Code and GitHub to create a version control of my program and the repository of my program.  
+  
+## Authors
+
+- [@rbkgarcia](https://github.com/rbkgarcia)
--- tic-tac-toe-game.py
+++ tic-tac-toe-game.py
@@ -5,10 +5,10 @@ Course: CSE 210
 '''
 class Board():
     def __init__(self):
-        self.board = "1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9"
-   
-    def display_board(self):
-        print(self.board+"\n")
+        self.board = "1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9\n"
+        
+    def show_board(self):
+        print(self.board)
         
     def update(self,new_board):
         self.board = new_board
@@ -22,7 +22,7 @@ class Game():
     def change_board(self, pos, value):
         new_board = self.board.board.replace(pos,value)
         self.board.update(new_board)
-        self.board.display_board()
+        self.board.show_board()
 
     def win_player(self):
         if((self.board.board[0]==self.board.board[2]==self.board.board[4]) or (self.board.board[0]==self.board.board[12]==self.board.board[24]) or (self.board.board[12]==self.board.board[14]==self.board.board[16]) or (self.board.board[24]==self.board.board[26]==self.board.board[28]) or (self.board.board[0]==self.board.board[2]==self.board.board[4]) or (self.board.board[2]==self.board.board[14]==self.board.board[26]) or (self.board.board[4]==self.board.board[16]==self.board.board[28])):
@@ -30,18 +30,18 @@ class Game():
 
     def keep_playing(self):
         while((self.counter!=0) and not(self.win_player())):
-            move = 0
+            position = 0
             if (self.counter%2 != 0):
-                move = input("x's turn to choose a square (1-9): ")
-                self.change_board(move, 'x')
+                position = input("x's turn to choose a square (1-9): ")
+                self.change_board(position, 'x')
             else: 
-                move = input("0's turn to choose a square (1-9): ") 
-                self.change_board(move, 'o')
+                position = input("0's turn to choose a square (1-9): ") 
+                self.change_board(position, 'o')
             self.counter = self.counter - 1 
         print("Good game. Thanks for playing!")
 
     def start_playing(self):
-        self.board.display_board()
+        self.board.show_board()
         self.keep_playing()
 
 def main():
